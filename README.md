e8a59568737a83a88cad682ddfa42d7e5766ab34
Mario Laurich <info@webnoob.de>
2019-05-16 10:01:43 +0200

    initial

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vuejs-signinup
 
+- [Demonstration on Youtube](https://youtu.be/uotY_cx1dvo)
+
 ## Project setup
 ```
 npm install
@@ -25,5 +27,6 @@ npm run test
 npm run lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## License
+
+[MIT](LICENSE)
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SignIn />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignIn from './components/SignIn.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SignIn
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: Tahoma;
+    font-size: 1.6rem;
+    color: #222;
+    background-color: #092525;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
